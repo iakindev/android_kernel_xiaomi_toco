516ca3220f53eb825d0889bf4dc6d52793bff5af
İlyas Akın <ashnwor@gmail.com>
2021-03-11 22:45:12 +0300

    Leverage more on LLVM utils

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 export ARCH=arm64
 export SUBARCH=arm64
 export DTC_EXT=dtc
+export KBUILD_COMPILER_STRING="$($HOME/development/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
 echo CLEAN OUT
 rm out -rf
@@ -16,4 +17,9 @@ PATH="$HOME/development/proton-clang/bin:$PATH" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- | tee kernel.log
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip | tee kernel.log
