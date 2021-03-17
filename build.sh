992e6cc407d2ea9deb1e1e476cd016257d78d983
İlyas Akın <ashnwor@gmail.com>
2021-03-17 22:31:57 +0300

    Build: Generate flashable zip

--- build.sh
+++ build.sh
@@ -1,20 +1,32 @@
+#!/bin/sh
+
 export ARCH=arm64
 export SUBARCH=arm64
 export DTC_EXT=dtc
 export KBUILD_COMPILER_STRING="$($HOME/development/proton-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-echo CLEAN OUT
-rm out -rf
+COMPRESSION="gz"
+OUT_ZIP="toco-experimentum-4-14-225"
+THREAD_COUNT=$(($(nproc --all)-1)) # Keep minus 1 for responsive desktop experience while building on relatively old systems.
+
+echo --CLEAN OUT
+echo - cleaning out
+rm -rf out
+echo - cleaning generated Image
+rm _anykernel/Image.$COMPRESSION-dtb
+echo - cleaning generated zip
+rm $OUT_ZIP
+echo - generate out directory
 mkdir out
 
 echo DEFCONFIG
-make  O=out ARCH=arm64 toco_user_defconfig #$DEFCONFIG
+make  O=out ARCH=arm64 toco_user_defconfig
 
 echo MENUCONFIG
 make O=out menuconfig
 
 PATH="$HOME/development/proton-clang/bin:$PATH" \
-make -j$(nproc --all) O=out \
+make -j$THREAD_COUNT O=out \
                       ARCH=arm64 \
                       CC=clang \
                       CROSS_COMPILE=aarch64-linux-gnu- \
@@ -24,3 +36,7 @@ make -j$(nproc --all) O=out \
                       OBJDUMP=llvm-objdump \
                       STRIP=llvm-strip \
                       LD=ld.lld | tee kernel.log
+
+cp out/arch/arm64/boot/Image.$COMPRESSION-dtb _anykernel/
+
+(cd _anykernel; zip -r ../toco-experimentum.zip .)
