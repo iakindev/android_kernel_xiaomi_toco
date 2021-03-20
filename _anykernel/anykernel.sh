d9107043d4ba5b2fcf1868f648573bb744a14952
İlyas Akın <ashnwor@gmail.com>
2021-03-21 01:35:22 +0300

    Build: Update anykernel kernel string

--- _anykernel/anykernel.sh
+++ _anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=toco-experimentum-4.14.225
+kernel.string=toco-experimentum-4.14.226
 do.devicecheck=1
 do.modules=0
 do.systemless=1
